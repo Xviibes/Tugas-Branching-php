315bf1cc9f8cf2842c61dcfb840199b8af480f69
Xviibes <134538998+Xviibes@users.noreply.github.com>
2023-11-19 04:04:20 -1200

    Update Akmal Xu-3 Tugas Branching.php

--- Akmal Xu-3 Tugas Branching.php
+++ Akmal Xu-3 Tugas Branching.php
@@ -3,28 +3,28 @@
 $nilai = 0;
 
 if ($nilai >= 81 and $nilai <= 100) {
-    echo"Nilai anda $nilai, nilai anda A berbobot 4";
+    echo"Nilai anda $nilai, nilai anda A bobot 4";
 }
 elseif ($nilai >= 78 and $nilai < 80.99) {
-    echo"Nilai anda $nilai, nilai anda A- berbobot 3.7";
+    echo"Nilai anda $nilai, nilai anda A- bobot 3.7";
 }
 elseif ($nilai >= 75 and $nilai < 77.99) {
-    echo"Nilai anda $nilai, nilai anda B+ berbobot 3.3";
+    echo"Nilai anda $nilai, nilai anda B+ bobot 3.3";
 }
 elseif ($nilai >= 70 and $nilai < 74.99) {
-    echo"Nilai anda $nilai, nilai anda B berbobot 3";
+    echo"Nilai anda $nilai, nilai anda B bobot 3";
 }
 elseif ($nilai >= 65 and $nilai < 69.99) {
-    echo"Nilai anda $nilai, nilai anda B- berbobot 2.7";
+    echo"Nilai anda $nilai, nilai anda B- bobot 2.7";
 }
 elseif ($nilai >= 60 and $nilai < 64.99) {
-    echo"Nilai anda $nilai, nilai anda C+ berbobot 2.3";
+    echo"Nilai anda $nilai, nilai anda C+ bobot 2.3";
 }
 elseif ($nilai >= 55 and $nilai < 59.99) {
-    echo"Nilai anda $nilai, nilai anda C berbobot 2";
+    echo"Nilai anda $nilai, nilai anda C bobot 2";
 }
 elseif ($nilai >= 40 and $nilai < 54.99) {
-    echo"Nilai anda $nilai, nilai anda D berbobot 1";
+    echo"Nilai anda $nilai, nilai anda D bobot 1";
 }
-else echo"Nilai and $nilai, nilai anda E berbobot 0, Silahkan coba kembali semester depan";
+else echo"Nilai and $nilai, nilai anda E bobot 0, Belajar bro jangan MALAS!!!";
 ?>
